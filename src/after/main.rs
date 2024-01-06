fa6be366fde9a5a25c123e7ce217765e12ae4f17
Jason Wilkes <notarealdeveloper@gmail.com>
2024-01-06 10:47:59 -0800

    Allow reading from a file or from stdin

--- src/after/main.rs
+++ src/after/main.rs
@@ -1,21 +1,20 @@
 use std::env;
 use std::io;
 use std::path;
+use std::fs;
 use regex::Regex;
 
-fn main() {
+fn print_usage_and_exit() {
     let argv: Vec<String> = env::args().collect();
+    let prog = path::Path::new(&argv[0]).file_name().unwrap();
+    eprintln!("usage: {:?} <pattern> [file]", prog);
+    std::process::exit(1);
+}
 
-    if argv.len() < 2 {
-        let prog = path::Path::new(&argv[0]).file_name().unwrap();
-        eprintln!("usage: {:?} <pattern>", prog);
-        std::process::exit(1);
-    }
-
+fn process_lines<R: io::BufRead>(reader: R, re: &Regex) {
     let mut on = false;
-    let re = Regex::new(&argv[1]).unwrap();
 
-    for line in io::stdin().lines() {
+    for line in reader.lines() {
         let s = line.unwrap();
         if on {
             println!("{}", s);
@@ -24,4 +23,30 @@ fn main() {
             on = true;
         }
     }
+}
+
+fn main() {
+    let argv: Vec<String> = env::args().collect();
+
+    let pattern = &argv[1];
+    let re = Regex::new(pattern).unwrap();
+
+    match argv.len() {
+
+        1 => print_usage_and_exit(),
+
+        2 => {
+            let stdin = io::stdin();
+            let reader = stdin.lock();
+            process_lines(reader, &re);
+        },
+
+        3 => {
+            let file = fs::File::open(&argv[2]).unwrap();
+            let reader = io::BufReader::new(file);
+            process_lines(reader, &re);
+        },
+
+        _ => print_usage_and_exit(),
+    };
 }
